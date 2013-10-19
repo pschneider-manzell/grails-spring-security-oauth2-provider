c81a5ceaca5a2e46d333d4d1f3545721f56fb196
Peter Schneider-Manzell <peter@schneider-manzell.de>
2013-10-19 12:02:27 +0200

    Fixed spring security config version and corresponding excludes

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,29 @@
-grails.work.dir = "target/workdir"
-grails.project.class.dir = 'target/classes'
-grails.project.test.class.dir = 'target/test-classes'
-grails.project.test.reports.dir	= 'target/test-reports'
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 grails.project.docs.output.dir = 'target/docs' // for backwards-compatibility, the docs are checked into gh-pages branch
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 
 grails.release.scm.enabled = false
+
+grails.project.dependency.resolver = "maven"
+
+grails.project.fork = [
+        // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+        //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+        // configure settings for the test-app JVM, uses the daemon by default
+        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        // configure settings for the run-app JVM
+        run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the run-war JVM
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the Console UI JVM
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
 
 // Code Narc
 codenarc.reports = {
@@ -33,15 +52,22 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
+
+
 //		compile('org.springframework.security:spring-security-crypto:3.1.4.RELEASE') {
 //			excludes 'spring-core', 'commons-logging'
 //		}
 		compile 'org.springframework.security.oauth:spring-security-oauth2:1.0.5.RELEASE', {
 			transitive = false
 		}
-		compile 'org.springframework.security:spring-security-config:3.0.7.RELEASE', {
-			excludes 'spring-core', 'spring-context', 'spring-aop', 'spring-web',
-					'aspectjweaver', 'servlet-api', 'commons-logging', 'aopalliance'
+		compile 'org.springframework.security:spring-security-config:3.2.0.RC1', {
+            excludes 'aopalliance', 'commons-codec', 'commons-logging', 'fest-assert', 'groovy', 'hsqldb',
+                    'jcl-over-slf4j', 'junit', 'logback-classic', 'mockito-core', 'powermock-api-mockito',
+                    'powermock-api-support', 'powermock-core', 'powermock-module-junit4',
+                    'powermock-module-junit4-common', 'powermock-reflect', 'spock-core', 'spring-aop',
+                    'spring-beans', 'spring-context', 'spring-core', 'spring-expression', 'spring-jdbc',
+                    'spring-security-core', 'spring-test', 'spring-tx', 'spring-web', 'spring-webmvc',
+                    'tomcat-servlet-api'
 		}
 	}
 	
